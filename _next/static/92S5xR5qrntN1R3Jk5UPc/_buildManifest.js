73666a66064b60b44a0c6b1548a7def143d63aaa
anismiles <32766+anismiles@users.noreply.github.com>
2026-07-23 00:03:48 +0000

    Update dashboard-artifact to output generated at 4bc77f9

---
+++ _next/static/92S5xR5qrntN1R3Jk5UPc/_buildManifest.js
@@ -0,0 +1,90 @@
+self.__BUILD_MANIFEST = {
+  "/": [
+    "static/chunks/0iz8gqf5fu0d_.js"
+  ],
+  "/_error": [
+    "static/chunks/0dk_crv70-idb.js"
+  ],
+  "/await_tree": [
+    "static/chunks/0e_sc22en6wdv.js"
+  ],
+  "/batch_tasks": [
+    "static/chunks/10bdqq-vop5xz.js"
+  ],
+  "/cluster": [
+    "static/chunks/0i7c7y13lhx4u.js"
+  ],
+  "/explain_distsql": [
+    "static/chunks/0c7fncmnnnh2p.js"
+  ],
+  "/fragment_graph": [
+    "static/chunks/0zaoirrd-0~zl.js"
+  ],
+  "/functions": [
+    "static/chunks/0hytavkhz5~1y.js"
+  ],
+  "/heap_profiling": [
+    "static/chunks/0g8nxkzo~rde7.js"
+  ],
+  "/indexes": [
+    "static/chunks/11c5l4grhwxo_.js"
+  ],
+  "/internal_tables": [
+    "static/chunks/00c5d3eg4s8p5.js"
+  ],
+  "/materialized_views": [
+    "static/chunks/0jd7b2oljkwqd.js"
+  ],
+  "/node": [
+    "static/chunks/0rgkgjyrmwz9z.js"
+  ],
+  "/relation_graph": [
+    "static/chunks/0gs8hjb_l-khu.js"
+  ],
+  "/settings": [
+    "static/chunks/18amoa7z9h9qw.js"
+  ],
+  "/sinks": [
+    "static/chunks/0ipf_3w6nqjhx.js"
+  ],
+  "/sources": [
+    "static/chunks/0nqi-3ur56btk.js"
+  ],
+  "/subscriptions": [
+    "static/chunks/08jfuyi48cb-7.js"
+  ],
+  "/tables": [
+    "static/chunks/0g2f56-hp57lk.js"
+  ],
+  "/views": [
+    "static/chunks/161eeez6sd.w9.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/",
+    "/_app",
+    "/_error",
+    "/await_tree",
+    "/batch_tasks",
+    "/cluster",
+    "/explain_distsql",
+    "/fragment_graph",
+    "/functions",
+    "/heap_profiling",
+    "/indexes",
+    "/internal_tables",
+    "/materialized_views",
+    "/node",
+    "/relation_graph",
+    "/settings",
+    "/sinks",
+    "/sources",
+    "/subscriptions",
+    "/tables",
+    "/views"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
